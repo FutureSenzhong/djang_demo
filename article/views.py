cf2ab6b3374bede77d6df8c8430208316678962c
sz <dengsz@ik.com>
2019-09-16 14:05:48 +0800

    改写hello django视图函数，使用模板渲染

--- article/views.py
+++ article/views.py
@@ -1,10 +1,21 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 # 编写第一个Hello Django视图程序
-def article_list(request):
-    return HttpResponse('Hello Django')
+from article.models import ArticlePost
 
+
+def article_list(request):
+    # hello django
+    # return HttpResponse('Hello Django')
+
+    # 查询取出所有的文章
+    articles = ArticlePost.objects.all()
+
+    # 数据组合成json传递给templates模板使用
+    data = {'articles': articles}
+
+    # 渲染模板文件展示数据
+    return render(request,  'article/list.html', data)
