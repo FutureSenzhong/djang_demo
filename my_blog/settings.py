a571dad7f25d4ff640ace48c50584ecaecc18ea7
sz <dengsz@ik.com>
2019-09-16 14:18:44 +0800

    添加bootstrap的css和js等静态文件

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -118,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# 静态文件配置
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
