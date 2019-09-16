6c1efa05569c128f9d12218a013f3b2c09762b64
sz <dengsz@ik.com>
2019-09-16 19:27:42 +0800

    添加登陆视图和页面

---
+++ user/urls.py
@@ -0,0 +1,42 @@
+"""my_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+# 正则部署的应用名称,Django2.0之后，app的urls.py必须配置app_name，否则会报错。
+from user import views
+
+app_name = 'user'
+
+# article应用的路由配置
+urlpatterns = [
+    # 文章列表
+    path('article-list/', views.article_list, name='article_list'),
+
+    # 文章详情页面
+    path('article-detail/<int:article_id>/', views.article_detail, name='article_detail'),
+
+    # 写文章
+    path('article-create/', views.article_create, name='article_create'),
+
+    # 删除文章
+    # path('article-delete/<int:article_id>/', views.article_delete, name='article_delete'),
+
+    # 安全删除文章
+    path('article-safe-delete/<int:article_id>/', views.article_safe_delete, name='article_safe_delete'),
+
+    # 更新文章
+    path('article-update/<int:article_id>/', views.article_update, name='article_update'),
+]
