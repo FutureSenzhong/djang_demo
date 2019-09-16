2eabeccf87340cfad69e956b892c667d128521fd
sz <dengsz@ik.com>
2019-09-16 13:55:16 +0800

    在article/admin.py中注册ArticlePost模型，告诉django后台admin模块，可以使用这个模型

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+# 我们需要“告诉”Django，后台中需要添加ArticlePost这个数据表供管理
+from article.models import ArticlePost
+
+admin.site.register(ArticlePost)
