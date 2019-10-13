b8424e484c156c07734ddfeae046694b0e89bbdc
“dengs” <1019217919@qq.com>
2019-10-13 16:41:02 +0800

    添加用户模型，分类，标签等模型

--- article/models.py
+++ article/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 
 # Create your models here.
@@ -14,28 +14,82 @@ from django.utils import timezone
 from mdeditor.fields import MDTextField
 
 
+class UserInfo(AbstractUser):
+    """
+    用户信息
+    """
+    id = models.AutoField(primary_key=True, serialize=False, verbose_name='ID')
+    telephone = models.CharField(max_length=11, null=True, unique=True, verbose_name='用户电话')
+    avatar = models.FileField(default="images/favicon.ico", verbose_name='用户头像')
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    blog = models.OneToOneField(to='Blog', to_field='id', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
+
+
+class Blog(models.Model):
+    """
+    博客信息
+    """
+    id = models.AutoField(primary_key=True, serialize=False, verbose_name='ID')
+    title = models.CharField(verbose_name='个人博客标题', max_length=255)
+    site_name = models.CharField(verbose_name='站点名称', max_length=255)
+    theme = models.CharField(verbose_name='博客主题', max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    """
+    文章分类表
+    """
+    id = models.AutoField(primary_key=True, serialize=False, verbose_name='ID')
+    title = models.CharField(verbose_name='分类标题', max_length=32)
+    article = models.ForeignKey(verbose_name='所属博客', to='ArticlePost', to_field='id', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    """
+    文章标签
+    """
+    id = models.AutoField(primary_key=True, serialize=False, verbose_name='ID')
+    title = models.CharField(verbose_name='标签名称', max_length=32)
+    blog = models.ForeignKey(verbose_name='所属博客', to='ArticlePost', to_field='id', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
 class ArticlePost(models.Model):
-    # 作者
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    """
+    博客文章
+    """
 
-    # 标题
-    title = models.CharField(max_length=255)
-
+    id = models.AutoField(primary_key=True, serialize=False, verbose_name='ID')
+    author = models.ForeignKey(verbose_name='作者', to='UserInfo', to_field='id', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='文章标题')
+    desc = models.CharField(max_length=255, verbose_name='文章描述')
+    comment_count = models.IntegerField(default=0, verbose_name='评论数量')
+    up_count = models.IntegerField(default=0, verbose_name='点赞数')
+    down_count = models.IntegerField(default=0, verbose_name='踩数')
+    image_count = models.IntegerField(default=0, verbose_name='封面图片数量')
+    category = models.ForeignKey(to='Category', to_field='id', on_delete=models.CASCADE)
+    # through参数可以指定用作中介的中间模型
+    tags = models.ManyToManyField(to="Tag", through='ArticleTag', null=True)
     # 正文
-    body = MDTextField()
-
-    # 单个图片
-    image = models.ImageField(null=True, blank=True)
-
-    # 批量上传的图片
-    image2 = models.ImageField(null=True, blank=True)
-    image3 = models.ImageField(null=True, blank=True)
-
-    # 创建时间
-    created = models.DateTimeField(default=timezone.now)
-
-    # 更新时间，自动更新auto_now=True
-    updated = models.DateTimeField(auto_now=True)
+    body = MDTextField(verbose_name='文章正文')
+    # 封面图片
+    image = models.ImageField(null=True, blank=True, verbose_name='封面图片1')
+    image2 = models.ImageField(null=True, blank=True, verbose_name='封面图片2')
+    image3 = models.ImageField(null=True, blank=True, verbose_name='封面图片3')
+    created = models.DateTimeField(default=timezone.now, verbose_name='创建时间')
+    # 自动更新auto_now=True
+    updated = models.DateTimeField(auto_now=True, verbose_name='更新时间')
 
     # 内部类class Meta提供模型的元数据。
     # 元数据是“任何不是字段的东西”，
@@ -55,3 +109,52 @@ class ArticlePost(models.Model):
 
     def get_absolute_url(self):
         return 'article/article-detail/%d/' % self.id
+
+
+class ArticleTag(models.Model):
+    """
+    文章分类关系表
+    """
+    id = models.AutoField(primary_key=True, serialize=False, verbose_name='ID')
+    article = models.ForeignKey(verbose_name='文章', to="ArticlePost", to_field='id', on_delete=models.CASCADE)
+    tag = models.ForeignKey(verbose_name='标签', to="Tag", to_field='id', on_delete=models.CASCADE)
+
+    class Meta:
+        # 组合唯一约束
+        unique_together = [
+            ('article', 'tag'),
+        ]
+
+    def __str__(self):
+        return self.article.title + "---" + self.tag.title
+
+
+class ArticleUpDown(models.Model):
+    """
+    点赞表
+    """
+    id = models.AutoField(primary_key=True, serialize=False, verbose_name='ID')
+    user = models.ForeignKey(to='UserInfo', to_field='id',null=True, on_delete=models.CASCADE)
+    article = models.ForeignKey(to="ArticlePost", to_field='id', null=True, on_delete=models.CASCADE)
+    is_up = models.BooleanField(default=True)
+
+    class Meta:
+        # 组合唯一约束
+        unique_together = [
+            ('article', 'user'),
+        ]
+
+
+class Comment(models.Model):
+    """
+    评论表
+    """
+    id = models.AutoField(primary_key=True, serialize=False, verbose_name='ID')
+    article = models.ForeignKey(verbose_name='评论文章', to='ArticlePost', to_field='id', on_delete=models.CASCADE)
+    user = models.ForeignKey(verbose_name='评论者', to='UserInfo', to_field='id', on_delete=models.CASCADE)
+    content = models.CharField(verbose_name='评论内容', max_length=255)
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    parent_comment = models.ForeignKey(to='Comment', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
